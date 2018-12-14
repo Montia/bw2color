8e6d09c49dc5279a32045beeb6120f35fb89ee2b
lyd <leelddd@outlook.com>
2018-12-14 22:18:15 +0800

    add some tool script help run in server and keep tfrecord folder

---
+++ tools/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+WORK_DIR=/home/u22520/fromwin/bw2color
+
+# 1. run spider
+# 2. preprocess
+# 1. generate tfrecord
+# 1. train
+if [ $1 == 1 ];then
+    echo run spider $WORK_DIR/tools/spider.py
+    python tools/spider.py --path $WORK_DIR/data
+elif [ $1 == 2 ];then
+    echo preprocess
+    python tools/preprocess.py --data $WORK_DIR/data --save $WORK_DIR/out/crop
+elif [ $1 == 3 ];then
+    echo generate tfrecord
+    python generateds.py
+fi
+
---
+++ tools/upload.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+scp backward.py forward.py generateds.py test.py colfax:/home/u22520/fromwin/bw2color/
+scp -r tools/ colfax:/home/u22520/fromwin/bw2color/
